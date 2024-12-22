1a3355576622a4033091673248aeb294975e09b2
Jesus <jesusnmartinez21@gmail.com>
2024-12-22 10:30:00 -0600

    e2

---
+++ ejercicio2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 2</title>
+</head>
+<body>
+    <?php 
+    $edad=20;
+    $peso=90.25;
+    $nombre="Juan";
+    $existe=true;
+    echo "Variable Integer: ";
+    echo $edad;
+    echo "<br>";
+    echo "Variable double: ";
+    echo $peso;
+    echo "<br>";
+    echo "variable string: ";
+    echo $nombre;
+    echo "<br>";
+    echo "Variable boolean: ";
+    echo $existe;
+    ?>
+</body>
+</html>
