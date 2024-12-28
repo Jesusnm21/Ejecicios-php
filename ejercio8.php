5ab2f538f39b2717ca6f0479020ba25e8c81c609
Jesus <jesusnmartinez21@gmail.com>
2024-12-28 10:30:00 -0600

    e8

---
+++ ejercio8.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 8</title>
+</head>
+<body>
+    <form method="post" action="pag2.php">
+        <h1>Ingrese su nombre: </h1>
+        <input type="text" name="nombre">
+        <br> <br>
+        <input type="submit" value="confirmar">
+    </form>
+</body>
+</html>
